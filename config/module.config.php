6569371ec22bb79d59d83a07a539d02600d186c9
Toon Verwerft <toon.verwerft@phpro.be>
2014-04-08 17:41:33 +0200

    Add the mail plugin manager to the service manager

--- config/module.config.php
+++ config/module.config.php
@@ -6,10 +6,12 @@ return [
             'Phpro\MailManager\Adapter\ZendMailAdapter' => 'Phpro\MailManager\Adapter\Factory\ZendMailAdapter',
             'Phpro\MailManager\Service\BodyRenderer' => 'Phpro\MailManager\Service\Factory\BodyRenderer',
             'Phpro\MailManager\Service\MailManager' => 'Phpro\MailManager\Service\Factory\MailManager',
+            'Phpro\MailManager\Service\MailPluginManager' => 'Phpro\MailManager\Service\Factory\MailPluginManager',
         ],
         'aliases' => [
             'Phpro\MailManager' => 'Phpro\MailManager\Service\MailManager',
             'Phpro\MailManager\DefaultAdapter' => 'Phpro\MailManager\Adapter\ZendMailAdapter',
+            'Phpro\MailManager\PluginManager' => 'Phpro\MailManager\Service\MailPluginManager',
         ],
     ],
 
